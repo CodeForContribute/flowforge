9541ebc5e9d0da37eb844ed5e4a36160bc02bfe5
Raushan Singh <19raushaniiitu@gmail.com>
2026-01-27 02:45:47 +0530

    Add tests/unit/sampleTest.spec.ts

---
+++ tests/unit/sampleTest.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from '@playwright/test';
+
+// Example unit test
+
+describe('Sample Unit Test', () => {
+  it('should pass this sample test', async () => {
+    const value = true;
+    expect(value).toBe(true);
+  });
+});
